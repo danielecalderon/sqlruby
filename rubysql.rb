e63e4b234250594b66793aaa0fe97a824a2036ec
danielecalderon <daniel.calderon06@gmail.com>
2020-02-04 02:31:13 +0000

    Added a data base

--- rubysql.rb
+++ rubysql.rb
@@ -1,9 +1,9 @@
 require 'sqlite3'
-db = SQLite3::Database.new("filename.db")
+db = SQLite3::Database.new("rubysql.db")
 
 db.execute <<-SQL
     create table users(
       id integer primary key,
       email varchar,
-      password varchar
+      password varchar)
   SQL
